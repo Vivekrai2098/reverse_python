da884632782ee03628dca1d9c16fd49ef010723d
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2022-03-23 13:14:33 +0530

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,14 @@
+l=str(input("enter your string value we reverse "))
+sum=0
+k=0
+for i in l:
+    sum-=1
+    k-=1
+    u=l[sum]
+    print("the index value is:=",k,u)
+
+if i==u:
+    print("this is the PALINDROME")
+else:
+    print("not PALINDROME")
+
